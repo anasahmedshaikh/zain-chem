629defb2595ffef0d87567fe2a454f8a34d9f256
anasahmedshaikh <156065391+anasahmedshaikh@users.noreply.github.com>
2024-09-17 16:47:30 +0500

    fixed bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+import Image from "next/image"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { BookOpenIcon, GraduationCapIcon, NewspaperIcon } from "lucide-react"
+import me from '../images/zain.jpg'
 
 export default function Component() {
   
@@ -30,11 +32,11 @@ export default function Component() {
         <section className="w-full py-12 md:py-24 lg:py-32 bg-gray-100 dark:bg-gray-800">
           <div className="container px-4 md:px-6">
             <div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
-              <img
+              <Image
                 alt="Professor Zain Chem"
                 className="mx-auto aspect-square overflow-hidden rounded-xl object-cover object-center sm:w-full lg:-order-none"
                 height="550"
-                src="https://zainchem.wordpress.com/wp-content/uploads/2017/07/about.jpg"
+                src={me}
                 width="550"
               />
               <div className="flex flex-col justify-center space-y-4">
