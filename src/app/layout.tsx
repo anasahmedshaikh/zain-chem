bfa6e8ecad15d537335553fcf9928ff3a91c79c9
anasahmedshaikh <156065391+anasahmedshaikh@users.noreply.github.com>
2024-09-18 22:10:29 +0500

    commited to main

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import { SpeedInsights } from "@vercel/speed-insights/next"
-import { GoogleAnalytics } from '@next/third-parties/google'
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "../components/header";
 import Footer from "../components/footer";
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
   title: "Zain Chem",
@@ -20,12 +20,12 @@ export default function RootLayout({
       <head>
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       </head>
-      <GoogleAnalytics gaId="G-2W88CYM8D6" />
       <body>
         <Header/>
         {children}
         <Footer/>
         <SpeedInsights/>
+        <Analytics/>
         </body>
     </html>
   );
