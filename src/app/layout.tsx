d9a50a504b5849f9a3b9ff79119da0c679216b82
anasahmedshaikh <156065391+anasahmedshaikh@users.noreply.github.com>
2024-09-18 20:01:00 +0500

    Update layout.tsx
    forget to add gtag for next

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { GoogleAnalytics } from '@next/third-parties/google'
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "../components/header";
@@ -19,13 +20,7 @@ export default function RootLayout({
       <head>
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       </head>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-2W88CYM8D6"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments)}
-        gtag('js', new Date());
-        gtag('config', 'G-2W88CYM8D6');
-      </script>
+      <GoogleAnalytics gaId="G-2W88CYM8D6" />
       <body>
         <Header/>
         {children}
